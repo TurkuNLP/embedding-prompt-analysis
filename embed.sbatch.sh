e176ad7ef42b85ff94562244f2398e3dc6334947
Filip Ginter <filip.ginter@gmail.com>
2026-02-03 17:14:02 +0200

    modifying the scripts to not launch many small jobs, and run puhti

--- embed.sbatch.sh
+++ embed.sbatch.sh
@@ -3,22 +3,35 @@
 #SBATCH --job-name=emb
 #SBATCH --output=logs/emb.out
 #SBATCH --time=12:00:00
-#SBATCH --partition=gpusmall
-#SBATCH --gres=gpu:a100:1
+##SBATCH --partition=gpusmall
+##SBATCH --gres=gpu:a100:1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:v100:1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=20G
 #SBATCH --account=project_2000539
 
 # $1: shard index
 # $2: total number of shards
-# $3: model full name
-# $4: model short name for output directory
 
 module load pytorch
-source /scratch/project_2000539/filip/information-gap/venv-local/bin/activate
+source /scratch/project_2000539/filip/embedding-prompt-analysis/venv-local/bin/activate
 export HF_HOME=/scratch/project_2000539/filip/HF_CACHE
 mkdir -p logs
-mkdir -p embeddings/$4
+mkdir -p embeddings/multilingual-e5-large-instruct
+mkdir -p embeddings/Qwen3-Embedding-0.6B
 
-python3 compute_embeddings.py --data datasets/mteb_hotpotqa_corpus.jsonl --output embeddings/$4/emb_mteb_hotpotqa_corpus --batch 200 --shard-total $2 \
-    --shard-index $1 --model $3
+#E5
+
+### mteb_hotpotqa_corpus ### --- I already have this one for this model
+for dataset in  mteb_nq_corpus mteb_quora_corpus
+do
+    python3 compute_embeddings.py --data datasets/$dataset.jsonl --output embeddings/multilingual-e5-large-instruct/emb_$dataset --batch 200 --shard-total $2 --shard-index $1 --model intfloat/multilingual-e5-large-instruct 
+done
+
+# Qwen/Qwen3-Embedding-0.6B
+
+for dataset in mteb_hotpotqa_corpus mteb_nq_corpus mteb_quora_corpus
+do
+    python3 compute_embeddings.py --data datasets/$dataset.jsonl --output embeddings/Qwen3-Embedding-0.6B/emb_$dataset --batch 200 --shard-total $2 --shard-index $1 --model Qwen/Qwen3-Embedding-0.6B 
+done
